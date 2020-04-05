175a34798660212af8ec9c45c258d42a6e729bde
Caiê Leal <caieleals@gmail.com>
2020-04-05 17:59:29 -0300

    iniciando o projeto de pedidos

---
+++ src/main/java/com/sistemapedidospring/SistemapedidoApplication.java
@@ -0,0 +1,13 @@
+package com.sistemapedidospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemapedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemapedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemapedidospring/SistemapedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemapedidospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemapedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
